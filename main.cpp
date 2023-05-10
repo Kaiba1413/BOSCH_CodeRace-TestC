9beec77e855a2167d8f0beea171e9ca2159c9195
Ha The Viet <aiv1hc@bosch.com>
2023-05-10 13:47:56 +0700

    Extend the buggy application

--- main.cpp
+++ main.cpp
@@ -9,3 +9,28 @@ void foo(int x)
         printf("This is a simple vulnerability\n");
     }
 }
+
+int doTocTou(int *pInData, int *pInDataLen, int *pOutData, int *pOutDataLen)
+{
+    if ((*pInDataLen > 2000) || (*pOutDataLen > 2000))
+    {
+        return 1;
+    }
+
+    for (int i = 0; i < *pInDataLen; i++)
+        printf("%d\n", pInData[i]);
+
+    for (int i = 0; i < *pOutDataLen; i++)
+        printf("%d\n", pOutData[i]);
+}
+
+int main(void)
+{
+    foo(1000);
+    int x = 1000;
+    int y = 109;
+    int lenX = 12;
+    int lenY = 1999;
+    int err = doTocTou(&x, &lenX, &y, &lenY);
+    return 0;
+}
