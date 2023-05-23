45f6e8c1e35fed61ed4bd5e1014c68555741e3c1
gogocow <gogocow.hc@gmail.com>
2023-05-23 14:15:22 +0700

    commit

--- process_sec_scanning_output.sh
+++ process_sec_scanning_output.sh
@@ -2,14 +2,17 @@
 # A sample Bash script to catch the warning from Sec Scanning Tool
 # It can be extended to handle different outputs from cppcheck
 echo Start Processing Sec Scanning Output
-if [ $? -eq 0 ]
-then 
-  exit 1
-elif [ $? -eq "0"]
-then 
-  exit 1
-else
-  echo $?
-  echo no vulnerabilities
-fi
+# This is a comment
+echo "Hello, World!" # This is also a comment
+
+# if [ $? -eq 0 ]
+# then 
+#   exit 1
+# elif [ $? -eq "0"]
+# then 
+#   exit 1
+# else
+#   echo $?
+#   echo no vulnerabilities
+# fi
 
