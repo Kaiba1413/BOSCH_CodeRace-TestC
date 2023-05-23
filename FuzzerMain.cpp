42e52845cb43c099574ec882ab40def5fdf27531
gogocow <gogocow.hc@gmail.com>
2023-05-23 14:58:06 +0700

    commit

--- FuzzerMain.cpp
+++ FuzzerMain.cpp
@@ -5,6 +5,9 @@
  *      Author: gogoCow
  */
 
+#include <stdint.h>
+#include <stddef.h>
+
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     // Use data and size to test target program
