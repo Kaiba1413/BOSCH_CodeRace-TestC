55d7fd6d2bbf8fda9d8f5b9919ee88b2f7c3bf53
gogocow <gogocow.hc@gmail.com>
2023-05-23 14:48:55 +0700

    commit

--- .pipelines/FuzzerMain.cpp
+++ .pipelines/FuzzerMain.cpp
@@ -4,3 +4,11 @@
  *  Created on: May 23, 2023
  *      Author: gogoCow
  */
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    // Use data and size to test target program
+    // ...
+    
+    return 0;
+}
